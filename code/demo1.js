93b6a4966da3da15bf822e3d370b03ee842c083f
VladimiraTodorova <vladimira_todorova@abv.bg>
2023-09-06 12:41:31 +0300

    added demo1.js and demo2.js for test

---
+++ code/demo1.js
@@ -0,0 +1,6 @@
+function demo1 (num){
+    for ( let i = 1; i <= 5; i++){
+        console.log(i);
+    }
+}
+demo1 (5)
---
+++ code/demo2.js
@@ -0,0 +1,9 @@
+function demo2 (number){
+
+    while (number >= 1) {
+        console.log(number);
+        number--;
+        
+    }
+}
+demo2 (5)
