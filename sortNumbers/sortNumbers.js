123bfcc0acd8783c827bb522c0dec9a4b02ea059
VladimiraTodorova <vladimira_todorova@abv.bg>
2023-09-09 16:15:14 +0300

    create function SortNumbers
    sort numbers from max to min

---
+++ sortNumbers/sortNumbers.js
@@ -0,0 +1,8 @@
+      
+    function sortNumbers(n1, n2, n3) {
+        let nums = [n1, n2, n3];
+        let sortedNumber = nums.sort((a, b) => b - a);
+        console.log(sortedNumber.join('\n'));
+    }
+    sortNumbers(-2,1 ,3)
+     
